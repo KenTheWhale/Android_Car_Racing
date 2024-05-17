2bdbdbc73979f18034f6ee374d3682e8307db272
KenTheWhale <quocvdkse160940@fpt.edu.vn>
2024-05-17 10:26:39 +0700

    init file

---
+++ app/src/main/java/com/ken/carracing/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ken.carracing;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.main_activity);
+    }
+}
